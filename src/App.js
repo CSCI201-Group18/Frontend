94e19fead0ff54be2aac4e072c7cd0edb1d25e87
Lucas Hunter <lphunter@usc.edu>
2023-04-10 21:04:28 -0700

    Added routes to app.js, not working though

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 
 import "bootstrap";
-import Home from './screens/home';
-import HomeGuest from './screens/homeGuest'
+import Home from "./screens/home";
+import HomeGuest from "./screens/homeGuest";
 import Profile from "./screens/Profile";
 import VillageRegistered from "./components/VillageRegistered";
 import VillageUnregistered from "./components/VillageUnregistered";
@@ -11,27 +11,25 @@ import ParksideUnregistered from "./components/ParksideUnregistered";
 import EVKRegistered from "./components/EVKRegistered";
 import EVKUnregistered from "./components/EVKUnregistered";
 import LoginPage from "./components/LoginBanner";
-import {Router, Route, Routes} from "react-router-dom";
-import Profile from './screens/Profile'
+import { Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
     <Router>
       <Routes>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/evku" component={EVKUnregistered} />
-        <Route exact path="/evkr" component={EVKRegistered} />
-        <Route exact path="/parku" component={ParksideUnregistered} />
-        <Route exact path="/parkr" component={ParksideRegistered} />
-        <Route exact path="/vilu" component={VillageUnregistered} />
-        <Route exact path="/vilr" component={VillageRegistered} />
-        <Route exact path="/homer" component={Home} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/homeu" component={HomeGuest} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/evku" element={<EVKUnregistered />} />
+        <Route path="/evkr" element={<EVKRegistered />} />
+        <Route path="/parku" element={<ParksideUnregistered />} />
+        <Route path="/parkr" element={<ParksideRegistered />} />
+        <Route path="/vilu" element={<VillageUnregistered />} />
+        <Route path="/vilr" element={<VillageRegistered />} />
+        <Route path="/homer" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/homeu" element={<HomeGuest />} />
+        <Route path="*" element={<LoginPage />} />
       </Routes>
     </Router>
-    </>
   );
 }
 
