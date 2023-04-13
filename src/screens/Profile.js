8264e17955577c0482fa03522ca683249c1b24fd
Lucas Hunter <lphunter@usc.edu>
2023-04-13 16:14:47 -0700

    profile.js modifications

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,46 +1,50 @@
-import './Profile.css'
-import SquareButton from '../components/Buttons/SquareButton'
-import { Link } from 'react-router-dom'
+import "./Profile.css";
+import SquareButton from "../components/Buttons/SquareButton";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-
-
   return (
     <>
-        <div id='body-grid-container'>
-            <div id='left-banner'>
-                <img id='profile-pic' src='/profile.png' alt='profile-pic'/>
-                <button class='button-background' id='banner-icon-1'>{"EMAIL"}</button>
-                <Link href="homeGuest.js" id='banner-icon-2'><button class='button-background' >LOGOUT</button></Link>
-            </div>
-            <div id='reviewBox'>
-              <div class='reviewItem'>
-                <div class='reviewDate'>{"DATE"}</div>
-                <div class='reviewHall'>{"DINNING-HALL"}</div>
-                <div class='reviewFood'>{"FOOD-ITEM"}</div>
-                <div class='reviewStar'>{"STARS"}</div>
-                <div class='tagGrid'>
-                  <div class='tag'>{"THIS FOOD SUCKS"}</div>
-                  <div class='tag'>{"THIS FOOD SUCKS"}</div>
-                  <div class='tag'>{"THIS FOOD SUCKS"}</div>
-                  <div class='tag'>{"THIS FOOD SUCKS"}</div>
-                  <div class='tag'>{"THIS FOOD SUCKS"}</div>
-                </div>
-              </div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-              <div class='reviewItem'></div>
-            </div>
-            <div id="homeButton">
-              <Link href="home.js"><SquareButton type="home"/></Link>
-            </div>
+      <div id="body-grid-container">
+        <div id="left-banner">
+          <img id="profile-pic" src="/profile.png" alt="profile-pic" />
+          <button class="button-background" id="banner-icon-1">
+            {"EMAIL"}
+          </button>
+          <Link href="homeGuest.js" id="banner-icon-2">
+            <button class="button-background">LOGOUT</button>
+          </Link>
         </div>
+        <div id="reviewBox">
+          <div class="reviewItem">
+            <div class="reviewDate">{"DATE"}</div>
+            <div class="reviewHall">{"DINNING-HALL"}</div>
+            <div class="reviewFood">{"FOOD-ITEM"}</div>
+            <div class="reviewStar">{"STARS"}</div>
+            <div class="tagGrid">
+              <div class="tag">{"THIS FOOD SUCKS"}</div>
+              <div class="tag">{"THIS FOOD SUCKS"}</div>
+              <div class="tag">{"THIS FOOD SUCKS"}</div>
+              <div class="tag">{"THIS FOOD SUCKS"}</div>
+              <div class="tag">{"THIS FOOD SUCKS"}</div>
+            </div>
+          </div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+          <div class="reviewItem"></div>
+        </div>
+        <div id="homeButton">
+          <Link to="/">
+            <SquareButton type="home" />
+          </Link>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
